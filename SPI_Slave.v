1dde766a943d40a3902d0640d301538de3618a04
Neelakantam03 <yeripillineelakantam@gmail.com>
2025-11-28 00:03:44 +0530

    Add files via upload

---
+++ SPI_Slave.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 27.11.2025 18:48:02
+// Design Name: 
+// Module Name: SPI_Slave
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SPI_Slave(
+
+    );
+endmodule
